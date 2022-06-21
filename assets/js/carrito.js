e6244bf8143704488cc14a225db898485693e7f0
Lucia Frumboli <luciafrumboli@gmail.com>
2022-06-20 21:21:22 -0300

    arreglando errores

--- assets/js/carrito.js
+++ assets/js/carrito.js
@@ -3,16 +3,13 @@ let carrito;
 if (localStorage.getItem("carrito") != null) {
     carrito = JSON.parse(localStorage.getItem("carrito"));
     //for of y funcion para recorrer el storage
-    for (const producto of carrito) {
-        let = document.getElementById("tablabody").innerHTML += `
-      <tr id='fila${producto.id}'>
-        <td>${producto.cantidad}</td>    
-        <td>${producto.nombre}</td>
-        <td>${producto.precio}</td>
-        <td>${producto.precio*producto.cantidad}</td>
-        <td><button id="btnrem${producto.id}"><i class="fa-solid fa-trash-can"></i></button></td>
-    </tr>`;
-    }
+    renderizarCarrito();
+    carrito.forEach(producto => {
+        document.getElementById(`btnrem${producto.id}`).addEventListener("click", function () {
+            eliminarDelCarrito(producto.id);
+        });
+    });
+    console.log(carrito);
 } else {
     carrito = [];
 }
@@ -41,13 +38,6 @@ function imprimirElementosEnHTML(productos) {
                 <picture> <img class="product__card__img" src="${producto.img}"> </picture>
                 <div class="product__card__text">
                 <div class="product__unit">
-                <select name="Cant">
-                    <option id="btnunit${producto.cantidad}">1</option>
-                    <option id="btnunit${producto.cantidad}">2</option>
-                    <option id="btnunit${producto.cantidad}">3</option>
-                    <option id="btnunit${producto.cantidad}">4</option>
-                    <option id="btnunit${producto.cantidad}">5</option>
-                </select>
                 <p>${producto.peso.peso1}</p>
                 <p>${producto.precio}USD</p>
                 <button class="product__card__buy" id="btnadd${producto.id}">AGREGAR</button>
@@ -70,17 +60,28 @@ function agregarAlCarrito(productoNuevo) {
     if (index !== -1) {
         carrito[index].cantidad += 1
         renderizarCarrito()
+        console.log(carrito);
+        document.querySelector("#precio__total__texto").innerText=(`
+        PRECIO TOTAL: ${obtenerPrecioTotal()} USD`);
         swal("Listo!", `Agregaste otra unidad de ${productoNuevo.nombre} al carrito!`, "success");
+        carrito.forEach(producto => {
+            document.getElementById(`btnrem${producto.id}`).addEventListener("click", function () {
+                eliminarDelCarrito(producto.id);
+            });
+        });
     } else {
         carrito.push(productoNuevo);
         swal("Listo!", `Agregaste ${productoNuevo.nombre} al carrito!`, "success");
 
         localStorage.setItem("carrito", JSON.stringify(carrito));
         renderizarCarrito()
+        console.log(carrito);
+        document.querySelector("#precio__total__texto").innerText=(`
+        PRECIO TOTAL: ${obtenerPrecioTotal()} USD`);
 
-        carrito.forEach(productoNuevo => {
-            document.getElementById(`btnrem${productoNuevo.id}`).addEventListener("click", function () {
-                eliminarDelCarrito(productoNuevo.id);
+        carrito.forEach(producto => {
+            document.getElementById(`btnrem${producto.id}`).addEventListener("click", function () {
+                eliminarDelCarrito(producto.id);
             });
         });
     }
@@ -89,10 +90,31 @@ function agregarAlCarrito(productoNuevo) {
 
 //ELIMINAR DEL CARRITO
 function eliminarDelCarrito(id) {
-    let newCarrito = carrito.filter(producto => producto.id !== id)
-    carrito = newCarrito
-    localStorage.setItem("carrito", JSON.stringify(carrito));
-    renderizarCarrito()
+    swal({
+        title: "Estás segurx?",
+        text: "Vas a eliminar el producto del carrito",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        if (willDelete) {
+            let newCarrito = carrito.filter(producto => producto.id !== id)
+            carrito = newCarrito
+            localStorage.setItem("carrito", JSON.stringify(carrito));
+            renderizarCarrito();
+          swal("Listo! ha sido eliminado!", {
+            icon: "success",
+          });
+          document.querySelector("#precio__total__texto").innerText=(`
+        PRECIO TOTAL: ${obtenerPrecioTotal()} USD`);
+        } else {
+          swal("El producto NO ha sido eliminado!");
+        }
+      });
+    renderizarCarrito();
+    document.querySelector("#precio__total__texto").innerText=(`
+    PRECIO TOTAL: ${obtenerPrecioTotal()} USD`);
 }
 
 function renderizarCarrito() {
@@ -118,16 +140,32 @@ function obtenerPrecioTotal() {
     }
     return precioTotal;
 }
-precioTotal = obtenerPrecioTotal();
 
-document.getElementById("precio__total").innerHTML += `
+/*Ordenar mayor/menor/alfabeticamente
+        function ordenar() {
+            let seleccion = document.querySelector("#miSeleccion").value;
+            console.log(seleccion)
+            if (seleccion == "menor") {
+                productos.sort(function(a, b) {
+                    return a.precio - b.precio
+                });
+            } else if (seleccion == "mayor") {
+                productos.sort(function(a, b) {
+                    return b.precio - a.precio
+                });
+            } else if (seleccion == "alfabetico") {
+                productos.sort(function(a, b) {
+                    return a.nombre.localeCompare(b.nombre);
+                });
+            }
+            document.querySelector(".product__card").innerHTML="";
+            imprimirElementosEnHTML();
+        }
 
-        <div id="precio__total">
-            <p>PRECIO TOTAL: ${precioTotal} USD</p>
-        </div>`
-
-//function para mostrar valor dolar y luego multiplicarlo por el valor total para convertir a pesos
-//falta multiplicar x precio total
+        document.querySelector("#miSeleccion option[value='pordefecto']").setAttribute("selected", true);
+        document.querySelector("#miSeleccion").onchange=()=>ordenar();
+*/
+//funcion para mostrar valor dolar del dia
 let dolarVenta;
 
 async function obtenerValorDolar() {
@@ -142,3 +180,30 @@ async function obtenerValorDolar() {
 }
 
 obtenerValorDolar();
+
+// boton de finalizacion de compra
+
+document.getElementById("finalizar__compra").addEventListener("click", function () {
+    if  (carrito.length != 0) {
+        finalizarCompra();
+        document.querySelector("#precio__total__texto").innerText=(`
+        PRECIO TOTAL: ${obtenerPrecioTotal()} USD`);
+    }
+    else {
+        noFinalizarCompra();
+    }
+    
+});
+
+function finalizarCompra() {
+    carrito=[];
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    renderizarCarrito();
+    document.querySelector("#precio__total__texto").innerText=(`
+    PRECIO TOTAL: ${obtenerPrecioTotal()} USD`);
+    swal ( " Finalizaste tu pedido " , " En instantes nos pondremos en contacto", "success" )   ;
+};
+
+function noFinalizarCompra() {
+    swal ( " El carrito está vacío " , " Agrega productos para comprar", "error" )   ;
+};
